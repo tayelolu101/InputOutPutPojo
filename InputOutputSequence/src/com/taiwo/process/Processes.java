1e2f363c0078897cea3a5410b6d6415abcc22b00
Ahmed Taiwo <t_whizzy101@yahoo.com>
2022-09-09 10:48:26 +0100

    Refactored code and added a switch case option

--- InputOutputSequence/src/com/taiwo/process/Processes.java
+++ InputOutputSequence/src/com/taiwo/process/Processes.java
@@ -28,28 +28,10 @@ public class Processes {
         if (result_from_inbox_file.trim().length() < 1){
             throw new customException( "Input file content is empty" );
         }
-        for (int i = 0; i < seqs.length; i++){
-            System.out.println("SEQ :" + seqs[i]);
-            if (seqs[i].trim().equalsIgnoreCase("1") || seqs[i].trim().equals("1")) {
-                System.out.println("Convert 7 to e starting ...........................");
-                result_from_inbox_file = convert_7_e(result_from_inbox_file);
-                System.out.println("Convert 7 to e ending ...........................");
-            }else if (seqs[i].trim().equalsIgnoreCase("2") || seqs[i].trim().equals("2")) {
-                System.out.println("Convert uppercase starting ...........................");
-                result_from_inbox_file = convert_to_upperCase(result_from_inbox_file);
-                System.out.println("Convert uppercase ending ...........................");
-            }else if (seqs[i].trim().equalsIgnoreCase("3") || seqs[i].trim().equals("3")) {
-                System.out.println("Remove whitespaces starting ...........................");
-                result_from_inbox_file = remove_white_spaces(result_from_inbox_file);
-                System.out.println("Remove whitespaces ending ...........................");
-            }else if (seqs[i].trim().equalsIgnoreCase("4") || seqs[i].trim().equals("4")) {
-                System.out.println("Covert to Ascii starting ...........................");
-                result_from_inbox_file = convert_to_ascii(result_from_inbox_file);
-                System.out.println("Covert to Ascii ending ...........................");
-            }else {
-                throw new customException("INVALID SEQUENCE NUMBER.............. : " + seqs[1]);
-            }
-        }
+        // To use for each
+        //result_from_inbox_file = for_each_option(seqs, result_from_inbox_file);
+        // To use switch case
+        result_from_inbox_file = switch_case_option(seqs, result_from_inbox_file);
         write_to_file(outboxPath, result_from_inbox_file);
     }
 
@@ -63,8 +45,7 @@ public class Processes {
             // Creating an object of BufferedReader class
             BufferedReader br = new BufferedReader(new FileReader(file));
             StringBuilder stringBuilder = new StringBuilder(input_file_string);
-            // Condition holds true till
-            // there is character in a string
+            // Condition holds true till there is character in a string
             while ((st = br.readLine()) != null) {
                 stringBuilder.append(st + System.lineSeparator());
             }
@@ -125,5 +106,63 @@ public class Processes {
             }
         }
         return stringBuilder.toString();
+    }
+
+    private String switch_case_option(String [] Seq, String res) throws customException {
+        System.out.println("Using the switchCase Method ...........................");
+        for(String seqs : Seq){
+            switch (seqs){
+                case "1" :
+                    System.out.println("Convert 7 to e starting ...........................");
+                    res = convert_7_e(res);
+                    System.out.println("Convert 7 to e ending ...........................");
+                    break;
+                case "2" :
+                    System.out.println("Convert uppercase starting ...........................");
+                    res = convert_to_upperCase(res);
+                    System.out.println("Convert uppercase ending ...........................");
+                    break;
+                case "3" :
+                    System.out.println("Remove whitespaces starting ...........................");
+                    res = remove_white_spaces(res);
+                    System.out.println("Remove whitespaces ending ...........................");
+                    break;
+                case "4" :
+                    System.out.println("Covert to Ascii starting ...........................");
+                    res = convert_to_ascii(res);
+                    System.out.println("Covert to Ascii ending ...........................");
+                    break;
+                default:
+                    throw new customException("INVALID SEQUENCE NUMBER.............. : " + seqs);
+            }
+        }
+        return res;
+    }
+
+    private String for_each_option(String [] seqs, String con) throws customException{
+        System.out.println("Using the forEach Method ...........................");
+        for (int i = 0; i < seqs.length; i++){
+            System.out.println("SEQ :" + seqs[i]);
+            if (seqs[i].trim().equalsIgnoreCase("1") || seqs[i].trim().equals("1")) {
+                System.out.println("Convert 7 to e starting ...........................");
+                con = convert_7_e(con);
+                System.out.println("Convert 7 to e ending ...........................");
+            }else if (seqs[i].trim().equalsIgnoreCase("2") || seqs[i].trim().equals("2")) {
+                System.out.println("Convert uppercase starting ...........................");
+                con = convert_to_upperCase(con);
+                System.out.println("Convert uppercase ending ...........................");
+            }else if (seqs[i].trim().equalsIgnoreCase("3") || seqs[i].trim().equals("3")) {
+                System.out.println("Remove whitespaces starting ...........................");
+                con = remove_white_spaces(con);
+                System.out.println("Remove whitespaces ending ...........................");
+            }else if (seqs[i].trim().equalsIgnoreCase("4") || seqs[i].trim().equals("4")) {
+                System.out.println("Covert to Ascii starting ...........................");
+                con = convert_to_ascii(con);
+                System.out.println("Covert to Ascii ending ...........................");
+            }else {
+                throw new customException("INVALID SEQUENCE NUMBER.............. : " + seqs[1]);
+            }
+        }
+        return con;
     }
 }
